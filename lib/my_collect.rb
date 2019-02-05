6928bfbec02e8c2e5c276c67d85b0197548bb363
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-05 05:44:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,17 +1,16 @@
 def my_collect(array)
-  new_collection = []
+  
   i = 0 
   while i < array.length 
-    new_collection << yield (array[i])
+    yield (array[i])
     i+=1
   end 
-  new_collection
 end
 
-my_collect(students) do |student|
-  student.split(" ").first 
+my_collect(array) do |student|
+  puts student.split(" ").first 
 end
 
-my_collection(languages) do |language|
-  language.upcase
+my_collection(array) do |language|
+  puts language.upcase
 end
